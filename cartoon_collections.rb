d41c000a7c05af2cc76e7d87f990ca6b365ba40b
Darkwah Boahen (Sam) <sdboahen@gmail.com>
2020-08-21 11:41:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,25 @@
-def greet_characters(array)
-  # Use `each` to enumerate over the provided array
-  #
-  # Print a custom greeting for each element
-end
+def 
+  
+  greet_characters(array)
 
-def list_dwarves(array)
-  # Use `each_with_index` to enumerate over the provided array
-  #
-  # Print a numbered list of each element
+
+  # `
+
 end
+# Use `each` to enumerate over the provided array
+#
+# Print a custom greeting for each element
+
+
+
+def 
+  
+  list_dwarves(array)
+
+
+  # `
+    
+end
+# Use `each_with_index` to enumerate over the provided array
+#
+# Print a numbered list of each element
