76456d5b36ecf7ae763c2e7fc996e8b553253f89
Darkwah Boahen (Sam) <sdboahen@gmail.com>
2020-08-21 12:23:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def
   greet_characters(array)
   
   array.each do |name| # <- More Appropriate |element| 
-    puts "Hello #{name}!"  end
+  puts "Hello #{name}!"  end
 
 end
 # Use `each` to enumerate over the provided array
@@ -16,8 +16,13 @@ def
   
   list_dwarves(array)
 
-
-  # `
+  number_for_list = 0
+  
+  array.each do |name| # <- More Appropriate |element| 
+  puts "#{number_for_list}. #{name}!" 
+  
+  number_for_list += 1
+            end
     
 end
 # Use `each_with_index` to enumerate over the provided array
